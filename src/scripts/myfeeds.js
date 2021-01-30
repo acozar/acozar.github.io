a2ef5a98a8a72b2136e81cef94777178de7fc31a
Hipertextos <hipertextos@gmail.com>
2021-01-30 20:16:17 +0100

    Raven gif and rss labels

--- src/scripts/myfeeds.js
+++ src/scripts/myfeeds.js
@@ -38,3 +38,25 @@
 			entryTemplate:'<article><h3><a href="{url}">{title}</a></h3><strong>{date}</strong><div>{shortBodyPlain}</div><em><small><a href="{url}">+</a></small></em></article>'
 		  })
 		})
+		jQuery(function($) {
+		  $("#rss-tesis").rss("http://blog.acozar.com/feeds/posts/default/-/Tesis",
+		  {
+			limit: 4,
+			dateFormat: 'D [de] MMMM',
+			entryTemplate:'<article><h3><a href="{url}">{title}</a></h3><strong>{date}</strong><div>{shortBodyPlain}</div><em><small><a href="{url}">+</a></small></em></article>'
+		  })
+		jQuery(function($) {
+		  $("#rss-antitesis").rss("http://blog.acozar.com/feeds/posts/default/-/Antitesis",
+		  {
+			limit: 4,
+			dateFormat: 'D [de] MMMM',
+			entryTemplate:'<article><h3><a href="{url}">{title}</a></h3><strong>{date}</strong><div>{shortBodyPlain}</div><em><small><a href="{url}">+</a></small></em></article>'
+		  })
+		jQuery(function($) {
+		  $("#rss-sintesis").rss("http://blog.acozar.com/feeds/posts/default/-/Sintesis",
+		  {
+			limit: 4,
+			dateFormat: 'D [de] MMMM',
+			entryTemplate:'<article><h3><a href="{url}">{title}</a></h3><strong>{date}</strong><div>{shortBodyPlain}</div><em><small><a href="{url}">+</a></small></em></article>'
+		  })
+		})
